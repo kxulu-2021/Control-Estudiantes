d9d39dfc9a87a5b74cd7975d2d7f41418737726f
Kevin Xulú <kxulu-2021348@kinal.edu.gt>
2023-02-22 21:33:37 -0600

    Primera Version

---
+++ middlewares/validar-roles.js
@@ -0,0 +1,31 @@
+const {request, response} = require('express');
+const esMaestroRole = (req = request, res = response, next) => {
+    if (!req.user) {
+        return res.status(500).json({
+            msg: 'Debes de iniciar sesion con tu cuenta'
+        })
+    }
+
+    const {role, nombre} = req.user
+    if(role != 'ROLE_MAESTRO'){
+        return res.status(401).json({
+            msg:  'Solo los profesores tienen permiso de hacer esto'
+        })
+    }
+
+
+
+    next();
+}
+
+const esAlumnoRole = (req = request, res = response) => {
+    if (!req.user) {
+        return res.status(500).json({
+            msg: 'Por favor inicia sesión con tu cuenta'
+        })
+    }
+}
+module.exports = {
+    esMaestroRole,
+    esAlumnoRole
+}
